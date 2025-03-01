bc49caea58f4df5539a8b1c6c55915ac78bbc7b9
Brandtechzz <brandonburke66@gmail.com>
2025-02-28 21:30:30 -0500

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,27 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom'; // Import react-router-dom components
+import Home from './components/Home'; // Import the Home component
+import About from './components/About'; // Import the About component
+import Contact from './components/Contact'; // Import the Contact component
+import Register from './components/Register/Register'; // Import the Register component
+import Login from './components/Login/Login'; // Import the Login component
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> // Route for Home
+        <Route path="/login" element={<Login />} /> // Route for Login
+        <Route path="/register" element={<Register />} /> // Route for Registration
+        <Route path="/about" element={<About />} /> // Route for About page
+        <Route path="/contact" element={<Contact />} /> // Route for Contact page
+        {/* Add additional routes as needed */}
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
+
