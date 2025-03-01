d1976f57d1e90886d17f28952f5f511bfb93bed6
Brandtechzz <brandonburke66@gmail.com>
2025-03-01 17:20:23 -0500

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+# Inline class to manage CarModel entries within CarMake
+class CarModelInline(admin.TabularInline): 
+    model = CarModel  # Specify the model to be used as inline
+    extra = 1  # Number of empty forms to display for adding new car models
 
-# Register your models here.
+# Admin class for CarModel
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'car_make', 'type')  # Fields to display in the admin list view
+    list_filter = ('year', 'car_make', 'type')  # Filter options for the admin list view
+    search_fields = ('name',)  # Searchable fields in the admin interface
 
-# CarModelInline class
+# Admin class for CarMake
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'established_year', 'description')  # Fields to display in the admin list view
+    search_fields = ('name',)  # Searchable fields for CarMake
+    inlines = [CarModelInline]  # Use inline class to manage CarModels with CarMake
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+# Registering models with their respective admin configurations
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
