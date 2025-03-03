d9d3947eb15f9809cea0c5177f7114bd51cea0bf
Brandtechzz <brandonburke66@gmail.com>
2025-03-02 23:31:44 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,15 +1,26 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path to register
+    path('register/', TemplateView.as_view(template_name="index.html"), name='register_template'),
+    
+    # path for registration
+    path('api/register/', views.registration, name='registration_api'),
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    # path for login view
+    path('login/', TemplateView.as_view(template_name="index.html")),
+
+    # path for logout view
+    path('logout/', TemplateView.as_view(template_name="index.html")),
+
+    # Path for login user API
+    path('api/login/', views.login_user, name='login_user'),  # API endpoint for login
 
     # path for dealer reviews view
 
